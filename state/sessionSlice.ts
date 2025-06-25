ee05e0eb6896f2c768c2903d43605f1d0ec831e6
dudialimurtado <dudimurtado19@gmail.com>
2025-06-25 17:45:29 +0700

    feat: create session slice

---
+++ state/sessionSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface SessionState {
+  token: string | null;
+}
+
+const initialState: SessionState = {
+  token: "1231",
+};
+
+const sessionSlice = createSlice({
+  name: "session",
+  initialState,
+  reducers: {
+    setToken: (state, action: PayloadAction<string>) => {
+      state.token = action.payload;
+    },
+    clearToken: (state) => {
+      state.token = null;
+    },
+  },
+});
+
+export const { setToken, clearToken } = sessionSlice.actions;
+export default sessionSlice.reducer;
