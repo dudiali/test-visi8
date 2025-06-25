d5953378f8802309c037afb2628a29b6b957cd83
dudialimurtado <dudimurtado19@gmail.com>
2025-06-25 17:36:56 +0700

    chore: setup library

---
+++ lib/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDcQETyn7mdPJxma1FK58mFRTZOsJePYXI",
+  authDomain: "auth-852d5.firebaseapp.com",
+  projectId: "auth-852d5",
+  storageBucket: "auth-852d5.appspot.com",
+  messagingSenderId: "674133951637",
+  appId: "1:674133951637:web:523f1ad021d58866d2479c",
+  measurementId: "G-MMXFNYSJ7M",
+};
